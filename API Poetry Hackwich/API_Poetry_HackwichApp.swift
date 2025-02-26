18c7943e184f5247ccb6b1f78ce2a8e5982d637a
Samantha Hiles <samih11@att.net>
2025-02-26 09:36:31 -0600

    Initial Commit

---
+++ API Poetry Hackwich/API_Poetry_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_Poetry_HackwichApp.swift
+//  API Poetry Hackwich
+//
+//  Created by Samantha Hiles on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct API_Poetry_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
